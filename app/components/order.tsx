26e3df0d2e295a8f9c4246cb87cb0fc456e48185
oprc <chisainux@gmail.com>
2025-07-03 17:46:56 +0700

    wake up 2

---
+++ app/components/order.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useState } from "react"
+import Swal from "sweetalert2"
+
+type OrderPeops = {
+    token: string
+}
+
+export default function Order(props:OrderPeops){
+    
+    const [file, setFile]= useState<File|null>(null)
+    
+    const [cnt, setCnt]= useState("")
+    const orderHandle = async (e:React.FormEvent) => {
+            e.preventDefault()
+            
+            var fd = new FormData()
+            fd.append('Content', cnt)
+            if (file != null) {
+                fd.append('File', file)
+            } else {
+                await Swal.fire("please fill all field");
+                return
+            }
+            // console.log(fd)
+            const res = await fetch("http://127.0.0.1:7200/order", {
+                method: "POST",
+                headers: {
+                    // "Content-Type":"application/json",
+                    "Authorization":props.token
+                },
+                body: fd
+            })
+
+            if (!res.ok) throw new Error("server error")
+            const data = await res.json()
+            console.log(data)
+            await Swal.fire({
+                "title": "Info",
+                "text": data,
+            })
+    
+        }
+    return (<div>
+            <h1>Order</h1>
+			<form onSubmit={orderHandle}>
+				<input type="file" name="file" id="" 
+                onChange={(e)=>{setFile(e.target.files?.[0] ?? null)}}/>
+				<input type="text" name="cnt" id="" placeholder="order" 
+				onChange={(e)=>{setCnt(e.target.value)}}/>
+				<input type="submit" value="Send" />
+			</form>
+            
+    </div>)
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,68 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import React, { useEffect, useState } from "react";
+import { Content } from "next/font/google";
+import Order from "./components/order";
+import Swal from "sweetalert2";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const [token, setToken] = useState("")
+	const [name, setName] = useState("")
+	const [pwd, setPwd] = useState("")
+	
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+	const handle = async (e:React.FormEvent) => {
+		e.preventDefault()
+		try {
+			const res = await fetch("http://127.0.0.1:7200/login", {
+				method: "POST",
+				headers: {
+					"Content-Type":"application/json"
+				},
+				body: JSON.stringify({
+					Name: name,
+					Password: pwd,
+				})
+			})
+
+			if (!res.ok) throw new Error("server error")
+			const data = await res.json()
+		localStorage.setItem("token", data)
+			setToken(data)
+
+		} catch(err) {
+			console.log(err)
+			setToken("")
+		}
+	}
+
+	
+
+	useEffect(()=>{
+		const localToken = localStorage.getItem("token")
+		if (localToken!= null)  {
+			setToken(localToken)
+		}
+	}, [])
+
+  return (
+	<div>
+		{ token == "" ? <div><p>Login</p>
+		<form onSubmit={handle}>
+			<input type="text" name="" id="" 
+			onChange={(e)=>{
+				setName(e.target.value)
+			}}/>
+			<input type="password" name="" id="" 
+			onChange={(e)=>{
+				setPwd(e.target.value)
+			}}/>
+			<input type="submit" value="" />
+		</form></div> : ""
+		}
+		<h1>{token}</h1>
+		<Order token={token}/>
+   	</div>
   );
 }
